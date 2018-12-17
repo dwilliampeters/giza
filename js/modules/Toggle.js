aad9caa55fc41fe4a2b6b3abb8abb37a2da9847e
Dapen <spartdan@live.co.uk>
2018-12-17 14:17:42 +0000

    targets

--- js/modules/Toggle.js
+++ js/modules/Toggle.js
@@ -1,18 +1,32 @@
 export default class Toggle {
-  constructor(el) {
+  constructor(el, options) {
+    const defaults = {
+      activeClass: "is-active",
+      target: ""
+    };
     this.el = el;
+    this.settings = Object.assign(defaults, options);
     this.init();
   }
 
   init() {
-    this.addEventListeners();
-  }
+    const toggle = () => {
+      let target = this.settings.target;
+      if (target === "") {
+        this.el.classList.toggle(this.settings.activeClass);
+      } else {
+        if (typeof target === "string") {
+          if (target === "parent") {
+            this.el.parentNode.classList.toggle(this.settings.activeClass);
+          } else {
+            document
+              .querySelector(target)
+              .classList.toggle(this.settings.activeClass);
+          }
+        }
+      }
+    };
 
-  addEventListeners() {
-    this.el.addEventListener("click", this.handleToggle);
-  }
-
-  handleToggle() {
-    this.classList.toggle("is-active");
+    this.el.addEventListener("click", toggle);
   }
 }
--- js/modules/index.js
+++ js/modules/index.js
@@ -8,8 +8,13 @@ const moduleElements = document.querySelectorAll("[data-module]");
 for (var i = 0; i < moduleElements.length; i++) {
   const el = moduleElements[i];
   const name = el.getAttribute("data-module");
+  let options = {};
+  const dataOptions = el.getAttribute("data-options");
+  if (dataOptions) {
+    options = JSON.parse(dataOptions);
+  }
   const Module = require(`./${name}`).default;
-  new Module(el);
+  new Module(el, options);
 }
 
 /*
