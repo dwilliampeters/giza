cdec128f500535e99b0a8d91b150e5c319e1857c
Daniel Peters <Daniel.Peters@jaywing.com>
2019-01-03 14:41:48 +0000

    update gulp

--- gulp/tasks/clean.js
+++
@@ -1,8 +0,0 @@
-const gulp = require("gulp");
-const clean = require("del").sync;
-
-const CONFIG = require("../config");
-
-gulp.task("clean", function() {
-  clean([CONFIG.DEST_BUILD, CONFIG.DEST_PROD]);
-});
