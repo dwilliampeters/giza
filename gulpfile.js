8513a7e341f6972654533b3dcac5546be1c32a0b
Dapen <spartdan@live.co.uk>
2019-01-18 16:53:47 +0000

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require("gulp");
 const gulpif = require("gulp-if");
-const gulpSequence = require("gulp-sequence");
 const path = require("path");
 const clean = require("del").sync;
 const nunjucksRender = require("gulp-nunjucks-render");
@@ -17,72 +16,70 @@ const port = process.env.SERVER_PORT || 3000;
 
 const CONFIG = require("./config");
 
+function projectPath(...paths) {
+  return path.resolve(process.env.INIT_CWD, ...paths);
+}
+
 let production = false;
 
 const html_paths = {
   src: [
-    `${CONFIG.BASE}/html/**/*.html`,
-    `!${CONFIG.BASE}/html/{components,layouts,shared,macros,data}/**`
+    projectPath(CONFIG.BASE, CONFIG.html.src, "**/*.html"),
+    "!" +
+      projectPath(
+        CONFIG.BASE,
+        CONFIG.html.src,
+        "**/{components,layouts,shared,macros,data}/**"
+      )
   ],
-  src_render: [`${CONFIG.BASE}/html`]
+  src_render: [projectPath(CONFIG.BASE, CONFIG.html.src)],
+  dest: projectPath(CONFIG.dest),
+  dist: projectPath(CONFIG.dist)
 };
 
-const sass_paths = {
-  src: [`${CONFIG.BASE}/scss/giza.scss`],
-  build: `${CONFIG.DEST_BUILD}/css`,
-  prod: `${CONFIG.DEST_PROD}/css`
+const stylesheets_paths = {
+  src: projectPath(CONFIG.BASE, CONFIG.stylesheets.src, "**/*.scss"),
+  dest: projectPath(CONFIG.dest, CONFIG.stylesheets.dest),
+  dist: projectPath(CONFIG.dist, CONFIG.stylesheets.dest)
 };
 
-const js_paths = {
-  src: [`${CONFIG.BASE}/js/giza.js`],
-  build: `${CONFIG.DEST_BUILD}/js`,
-  prod: `${CONFIG.DEST_PROD}/js`
+const javascripts_paths = {
+  src: projectPath(CONFIG.BASE, CONFIG.javascripts.src, "**/*.js"),
+  dest: projectPath(CONFIG.dest, CONFIG.javascripts.dest),
+  dist: projectPath(CONFIG.dist, CONFIG.javascripts.dest)
 };
 
 const images_paths = {
-  src: [`${CONFIG.BASE}/images/**/*{jpg,png,svg}`],
-  build: `${CONFIG.DEST_BUILD}/images`,
-  prod: `${CONFIG.DEST_PROD}/images`
+  src: projectPath(CONFIG.BASE, CONFIG.images.src, "**/*{jpg,png,svg}"),
+  dest: projectPath(CONFIG.dest, CONFIG.images.dest),
+  dist: projectPath(CONFIG.dist, CONFIG.images.dest)
 };
 
 const fonts_paths = {
-  src: [`${CONFIG.BASE}/fonts/**/*`],
-  build: `${CONFIG.DEST_BUILD}/fonts`,
-  prod: `${CONFIG.DEST_PROD}/fonts`
+  src: projectPath(CONFIG.BASE, CONFIG.fonts.src, "/**/*"),
+  dest: projectPath(CONFIG.dest, CONFIG.fonts.dest),
+  dist: projectPath(CONFIG.dist, CONFIG.fonts.dest)
 };
 
 const icons_paths = {
-  src: [`${CONFIG.BASE}/icons/*.svg`],
-  build: `${CONFIG.DEST_BUILD}/images`,
-  prod: `${CONFIG.DEST_PROD}/images`
-};
-
-const lab_html_paths = {
-  src: [
-    `${CONFIG.BASE}/lab/html/**/*.html`,
-    `!${CONFIG.BASE}/lab/html/{components,layouts,shared,macros,data}/**`
-  ],
-  src_render: [`${CONFIG.BASE}/lab/html`, `${CONFIG.BASE}/html`]
-};
-
-const lab_sass_paths = {
-  src: [`${CONFIG.BASE}/lab/scss/giza.scss`],
-  build: `${CONFIG.DEST_BUILD}/lab/css`
+  src: projectPath(CONFIG.BASE, CONFIG.icons.src, "*.svg"),
+  dest: projectPath(CONFIG.dest, CONFIG.icons.dest),
+  dist: projectPath(CONFIG.dist, CONFIG.icons.dest)
 };
 
 var webpackConfig = {
   mode: "development",
-  context: path.resolve("js/"),
+  context: path.resolve("javascripts/"),
   entry: {
     app: ["babel-polyfill", "giza.js"]
   },
   output: {
-    path: path.resolve("_build/js/"),
+    path: path.resolve("_build/site/javascripts/"),
     filename: "giza.js",
-    publicPath: "/js/"
+    publicPath: "/javascripts/"
   },
   resolve: {
-    modules: [path.resolve("js/"), path.resolve("node_modules")]
+    modules: [path.resolve("javascripts/"), path.resolve("node_modules")]
   },
   module: {
     rules: [
@@ -99,24 +96,24 @@ var webpackConfig = {
 };
 
 gulp.task("clean_build", function() {
-  clean([CONFIG.DEST_BUILD]);
+  clean([CONFIG.dest]);
 });
 
 gulp.task("clean_dist", function() {
-  clean([CONFIG.DEST_PROD]);
+  clean([CONFIG.dist]);
 });
 
 gulp.task("html", function() {
   return gulp
     .src(html_paths.src)
     .pipe(nunjucksRender({ path: html_paths.src_render }))
-    .pipe(gulpif(!production, gulp.dest(CONFIG.DEST_BUILD)))
-    .pipe(gulpif(production, gulp.dest(CONFIG.DEST_PROD)));
+    .pipe(gulpif(!production, gulp.dest(html_paths.dest)))
+    .pipe(gulpif(production, gulp.dest(html_paths.dist)));
 });
 
-gulp.task("sass", function() {
+gulp.task("stylesheets", function() {
   return gulp
-    .src(sass_paths.src)
+    .src(stylesheets_paths.src)
     .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(
@@ -132,109 +129,76 @@ gulp.task("sass", function() {
       ])
     )
     .pipe(sourcemaps.write("."))
-    .pipe(gulpif(!production, gulp.dest(sass_paths.build)))
-    .pipe(gulpif(production, gulp.dest(sass_paths.prod)));
+    .pipe(gulpif(!production, gulp.dest(stylesheets_paths.dest)))
+    .pipe(gulpif(production, gulp.dest(stylesheets_paths.dist)));
 });
 
-gulp.task("javascript", function() {
+gulp.task("javascripts", function() {
   return gulp
-    .src(js_paths.src)
+    .src(javascripts_paths.src)
     .pipe(webpackStream(webpackConfig, webpack))
-    .pipe(gulpif(!production, gulp.dest(js_paths.build)))
-    .pipe(gulpif(production, gulp.dest(js_paths.prod)));
+    .pipe(gulpif(!production, gulp.dest(javascripts_paths.dest)))
+    .pipe(gulpif(production, gulp.dest(javascripts_paths.dist)));
 });
 
 gulp.task("images", function() {
   return gulp
     .src(images_paths.src)
-    .pipe(gulpif(!production, gulp.dest(images_paths.build)))
-    .pipe(gulpif(production, gulp.dest(images_paths.prod)));
+    .pipe(gulpif(!production, gulp.dest(images_paths.dest)))
+    .pipe(gulpif(production, gulp.dest(images_paths.dist)));
 });
 
 gulp.task("fonts", function() {
   return gulp
     .src(fonts_paths.src)
-    .pipe(gulpif(!production, gulp.dest(fonts_paths.build)))
-    .pipe(gulpif(production, gulp.dest(fonts_paths.prod)));
+    .pipe(gulpif(!production, gulp.dest(fonts_paths.dest)))
+    .pipe(gulpif(production, gulp.dest(fonts_paths.dist)));
 });
 
 gulp.task("icons", function() {
   return gulp
     .src(icons_paths.src)
     .pipe(svgstore())
-    .pipe(gulpif(!production, gulp.dest(icons_paths.build)))
-    .pipe(gulpif(production, gulp.dest(icons_paths.prod)));
-});
-
-gulp.task("lab_html", function() {
-  return gulp
-    .src(lab_html_paths.src)
-    .pipe(nunjucksRender({ path: lab_html_paths.src_render }))
-    .pipe(gulp.dest(CONFIG.DEST_LAB_BUILD));
-});
-
-gulp.task("lab_sass", function() {
-  return gulp
-    .src(lab_sass_paths.src)
-    .pipe(sourcemaps.init())
-    .pipe(plumber())
-    .pipe(
-      sass({
-        includePaths: ["node_modules", "scss"]
-      }).on("error", sass.logError)
-    )
-    .pipe(
-      postcss([
-        autoprefixer({
-          browsers: ["last 2 versions", "ie >= 9", "Android >= 2.3", "ios >= 7"]
-        })
-      ])
-    )
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(lab_sass_paths.build));
+    .pipe(gulpif(!production, gulp.dest(icons_paths.dest)))
+    .pipe(gulpif(production, gulp.dest(icons_paths.dist)));
 });
 
 gulp.task("watch", function() {
-  gulp.watch(`${CONFIG.BASE}/html/**/*`, ["html", browser.reload]);
-  gulp.watch(`${CONFIG.BASE}/scss/**/*`, ["sass", browser.reload]);
-  gulp.watch(`${CONFIG.BASE}/js/**/*`, ["javascript", browser.reload]);
-  gulp.watch(`${CONFIG.BASE}/images/**/*`, ["images", browser.reload]);
-  gulp.watch(`${CONFIG.BASE}/fonts/*`, ["fonts", browser.reload]);
-  gulp.watch(`${CONFIG.BASE}/lab/html/**/*`, ["lab_html", browser.reload]);
-  gulp.watch(`${CONFIG.BASE}/lab/scss/**/*`, ["lab_sass", browser.reload]);
+  gulp.watch(html_paths.src, ["html", browser.reload]);
+  gulp.watch(stylesheets_paths.src, ["stylesheets", browser.reload]);
+  gulp.watch(javascripts_paths.src, ["javascripts", browser.reload]);
+  gulp.watch(images_paths.src, ["images", browser.reload]);
+  gulp.watch(fonts_paths.src, ["fonts", browser.reload]);
 });
 
 gulp.task("serve", ["build"], function() {
   browser.init({ server: "./_build", port: port, open: false });
 });
 
-gulp.task("serve_dist", function(cb) {
-  production = true;
-
-  gulpSequence(
-    "clean_dist",
-    "sass",
-    "html",
-    "javascript",
-    "images",
-    "fonts",
-    "icons",
-    cb
-  );
-});
-
 gulp.task("build", [
   "clean_build",
   "html",
-  "sass",
-  "javascript",
+  "stylesheets",
+  "javascripts",
   "images",
   "fonts",
-  "icons",
-  "lab_html",
-  "lab_sass"
+  "icons"
 ]);
+
+gulp.task("build_dist", function() {
+  production = true;
+
+  gulp.start([
+    "clean_build",
+    "html",
+    "stylesheets",
+    "javascripts",
+    "images",
+    "fonts",
+    "icons"
+  ]);
+});
 
 gulp.task("default", ["serve", "watch"]);
 
-gulp.task("dist", ["serve_dist"]);
+gulp.task("dist", ["build_dist"]);
