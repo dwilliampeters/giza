952844a90a2f4f6ea847a39d0afdcf3fa42ee39b
Dapen <spartdan@live.co.uk>
2019-01-23 09:22:06 +0000

    add grid prefixing

--- gulpfile.js
+++ gulpfile.js
@@ -124,6 +124,7 @@ gulp.task("stylesheets", function() {
     .pipe(
       postcss([
         autoprefixer({
+          grid: true,
           browsers: ["last 2 versions", "ie >= 9", "Android >= 2.3", "ios >= 7"]
         })
       ])
